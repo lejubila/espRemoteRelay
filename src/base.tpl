1c7fcda1a3e4fc1884d9d2a29165eadb7571ada2
david <david.bigagli@gmail.com>
2018-04-10 00:20:28 +0200

    Implementato gestione template per eseguire il rendering delle pagine web, visualizzato nomi dei relè in ordine alfabetico nella pagina web

---
+++ src/base.tpl
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>%head_title%</title>
+    </head>
+    <body>
+        <h1>%title%</h1>
+        <div class="container">
+            %body%
+        </div>
+    </body>
+</html>
--- src/httpserver.lua
+++ src/httpserver.lua
@@ -16,7 +16,7 @@ httpserver = {
         httpServer:use('/', function(req, res)
             local html_relay = ''
 
-            for relay_name, _ in pairs(config_relay) do
+            for relay_name, _ in pairsKeySorted(config_relay) do
                 if relay.relay_closed[relay_name] then
                     html_relay = html_relay .. "<li><a href='/close?relay=" .. relay_name .. "'>Close '" .. relay_name .. "'</a></li>"
                 else
@@ -24,40 +24,25 @@ httpserver = {
                 end
             end
 
-            res:send([[
-            <html>
-                <head>
-                    <title>espRemoteRelay</title>
-                </head>
-                <body>
-                    <h1>espRemoteRelay</h1>
-                    <div>
-                        <ul>
-                            ]]  .. html_relay .. [[
-                        </ul>
-                    </div>
-                </body>
-            </html>
-            ]]) -- /welcome?name=doge
+            data = {
+                head_title = 'espRemoteRelay',
+                title = 'espRemoteRelay',
+                body = '<ul>' .. html_relay .. '</ul>'
+            }
+
+            res:send( httpserver.tpl_render('base.tpl', data) )
         end)
 
         httpServer:use('/open', function(req, res)
             if req.query.relay and relay.close(req.query.relay) then
                 res:redirect('/')
             else
-                res:send([[
-                <html>
-                    <head>
-                        <title>espRemoteRelay</title>
-                    </head>
-                    <body>
-                        <h1>espRemoteRelay</h1>
-                        <div>
-                            Open Error
-                        </div>
-                    </body>
-                </html>
-                ]])
+                data = {
+                    head_title = 'espRemoteRelay',
+                    title = 'espRemoteRelay',
+                    body = '<p>Open Error</p><p><a href="/">Return to home</a></p>'
+                }
+                res:send( httpserver.tpl_render('base.tpl', data) )
             end
         end)
 
@@ -65,22 +50,29 @@ httpserver = {
             if req.query.relay and relay.open(req.query.relay) then
                 res:redirect('/')
             else
-                res:send([[
-                <html>
-                    <head>
-                        <title>espRemoteRelay</title>
-                    </head>
-                    <body>
-                        <h1>espRemoteRelay</h1>
-                        <div>
-                            Close Error
-                        </div>
-                    </body>
-                </html>
-                ]])
+                data = {
+                    head_title = 'espRemoteRelay',
+                    title = 'espRemoteRelay',
+                    body = '<p>Close Error</p><p><a href="/">Return to home</a></p>'
+                }
+                res:send( httpserver.tpl_render('base.tpl', data) )
             end
         end)
 
+    end,
+
+    tpl_render = function(tpl_file, data)
+        tpl = '';
+        if file.open(tpl_file) then
+            tpl = file.read()
+            file.close()
+        end
+
+        for k, v in pairs(data) do
+            tpl = tpl:gsub('%%'..k..'%%', v)
+        end
+
+        return tpl
     end
 
 }
